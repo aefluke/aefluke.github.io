03ab40627f26ee7efa7b8052e901fd807222bb36
ahmed <ahmet6yilmaz@gmail.com>
2019-04-23 18:44:40 +0300

    native node

---
+++ server.js
@@ -0,0 +1,11 @@
+var static = require('node-static');
+var http = require('http');
+
+var file = new(static.Server)();
+
+const args = require('minimist')(process.argv.slice(2))
+const port = args['port'] || 3000
+http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(port);
+console.log('Server started on port : '+ port);
